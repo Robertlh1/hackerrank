3f774126f190bf88c17f288ca2dba38d6e4b950e
Robert Hasel <rlhasel1@gmail.com>
2023-07-01 13:34:58 -0700

    Changed file structure, added In Progress and Completed folders

---
+++ Completed/Inserting a Node Into a Sorted Doubly Linked List/sortedInsert.js
@@ -0,0 +1,31 @@
+const DoublyLinkedListNode = require('../../Utility Functions/DoublyLinkedListNode')
+const doubleListBuilder = require('../../Utility Functions/doubleListBuilder')
+function sortedInsert(llist, data) {
+  // Write your code here
+  let newLList = llist
+  if (llist === null) return new DoublyLinkedListNode(data)
+  if (data < llist.data) {
+    llist.prev = new DoublyLinkedListNode(data)
+    llist.prev.next = llist
+    return llist.prev
+  }
+  while (true)  {
+    debugger
+      if (newLList.data > data) {
+          let insert = new DoublyLinkedListNode(data)
+          insert.prev = newLList.prev
+          insert.next = newLList
+          newLList.prev.next = insert
+          newLList.prev = insert
+          return llist
+      }
+      if (!newLList.next) {
+          newLList.next = new DoublyLinkedListNode(data)
+          newLList.next.prev = newLList
+          return llist
+      }
+      newLList = newLList.next
+  }
+}
+
+module.exports = sortedInsert
---
+++ Completed/Inserting a Node Into a Sorted Doubly Linked List/sortedInsert.test.js
@@ -0,0 +1,29 @@
+const sortedInsert = require('./sortedInsert')
+const doubleListBuilder = require('../../Utility Functions/doubleListBuilder')
+
+describe("Testing sortedInsert", () => {
+  test("sortedInsert() should return [1,2,3,4] for the list [1,2,4], 3", () => {
+    let head1 = doubleListBuilder([1,2,4])
+    let test = sortedInsert(head1, 3)
+    let result = doubleListBuilder([1,2,3,4])
+    expect(test).toEqual(result);
+  })
+  test("sortedInsert() should return [1,3,4,5,10] for the list [1,3,4,10], 5", () => {
+    let head1 = doubleListBuilder([1,3,4,10])
+    let test  = sortedInsert(head1, 5)
+    let result = doubleListBuilder([1,3,4,5,10])
+    expect(test).toEqual(result);
+  })
+  test("sortedInsert() should return [1,2,3,4] for the list [2,3,4], 1", () => {
+    let head1 = doubleListBuilder([2,3,4])
+    let test  = sortedInsert(head1, 1)
+    let result = doubleListBuilder([1,2,3,4])
+    expect(test).toEqual(result);
+  })
+  test("sortedInsert() should return [1,2,3,4] for the list [1,2,3], 4", () => {
+    let head1 = doubleListBuilder([1,2,3])
+    let test  = sortedInsert(head1, 4)
+    let result = doubleListBuilder([1,2,3,4])
+    expect(test).toEqual(result);
+  })
+})
