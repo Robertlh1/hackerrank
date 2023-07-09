41bf3121a5980f8f65a17ce277cadbdbaa822ba9
Robert Hasel <rlhasel1@gmail.com>
2023-07-08 21:11:23 -0700

    Completed Weighted Uniform Strings

--- Completed/Weighted Uniform Strings/weightedUniformStrings.js
+++ Completed/Weighted Uniform Strings/weightedUniformStrings.js
@@ -36,25 +36,25 @@ Now for each value in , see if it exists in the possible string weights. The ret
 
 function weightedUniformStrings(s, queries) {
   let codex = {a:1,b:2,c:3,d:4,e:5,f:6,g:7,h:8,i:9,j:10,k:11,l:12,m:13,n:14,o:15,p:16,q:17,r:18,s:19,t:20,u:21,v:22,w:23,x:24,y:25,z:26}
-  let weights = [], result = []
+  let weights = {}, result = []
 
   for (let i = 0; i < s.length; i++) {
     let currWeight = 0
     currWeight += codex[s[i]]
-    weights.push(currWeight)
+    weights[currWeight] = true
     let j = i + 1
     while (s[i] === s[j]) {
       currWeight += codex[s[i]]
-      weights.push(currWeight)
+      weights[currWeight] = true
       j++
     }
     i = j-1
   }
 
   for (let k = 0; k < queries.length; k++) {
-    if (weights.includes(queries[k])) result.push('Yes')
-    else result.push('No')
+    weights[queries[k]] ? result.push('Yes') : result.push('No')
   }
+
   return result
 }
 
